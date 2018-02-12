0b9ff31d8539c696f8a4b4d96c7af633015a89c4
kytt <kyttpwned@hotmail.com>
2018-02-12 20:58:52 +0700

    [ADD] source

---
+++ source.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<signal.h>
+#include<string.h>
+
+#define STR_LEN 100
+
+char **splitSpace(char str[]){
+	char** split = (char **) malloc(20 * sizeof(char *));
+	// Returns first token 
+    	char *token = strtok(str, " ");
+   	int i=0;
+    	// delimiters present in str[].
+    	while (token != NULL)
+    	{	
+		split[i] = token;
+		
+       		//printf("%s1", split[i]);
+        	token = strtok(NULL, " ");
+		i++;
+    	}
+	split[i++] = NULL;
+	return split;
+}
+void execute(char **cmd){
+	pid_t pid = fork();
+	if(!pid) {
+		if( execvp(cmd[0],cmd) >0){
+			execvp(cmd[0],cmd);
+			exit(0);
+		}
+		else
+			printf("\"%s\" command not found! \n",cmd);
+	}
+	else
+		wait();
+}
+
+int main(int argc, char *argv[])
+{	
+	char str[STR_LEN];
+	char **cmd;
+	while(1){
+
+		printf("prompt > ");
+		gets(str);
+		cmd = splitSpace(str);
+		if(!strcmp(cmd[0],"exit"))
+			break;
+		execute(cmd);
+		
+	}
+}
